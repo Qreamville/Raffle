2fa5f14e2c700d0a59eb96b3aabbdf45119e5a0e
Qreamville <Oyeleyebalikis@yahoo.com>
2022-09-16 02:43:17 -1200

    Update hardhat.config.ts

--- smart-contract/hardhat.config.ts
+++ smart-contract/hardhat.config.ts
@@ -3,24 +3,22 @@ import "@nomicfoundation/hardhat-toolbox";
 import "@nomiclabs/hardhat-etherscan";
 import "solidity-coverage";
 import "@typechain/hardhat";
-require("dotenv").config();
+import "dotenv/config";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
+  defaultNetwork: "hardhat",
   etherscan: {
     apiKey: process.env.ETHERSCAN_API_KEY || "",
   },
-  defaultNetwork: "hardhat",
   networks: {
+    hardhat: {
+      chainId: 31337,
+    },
     goerli: {
       url: process.env.GOERLI_RPC_URL || "",
       accounts: [process.env.PRIVATE_KEY || ""],
       chainId: 5,
-    },
-    rinkeby: {
-      url: process.env.RINKEBY_RPC_URL || "",
-      accounts: [process.env.PRIVATE_KEY || ""],
-      chainId: 4,
     },
   },
 };
