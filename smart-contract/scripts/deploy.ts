76259ea22a4d6230bb095db53630aea8c6eb2ee3
Qreamville <Oyeleyebalikis@yahoo.com>
2022-09-16 02:43:45 -1200

    Update deploy.ts

--- smart-contract/scripts/deploy.ts
+++ smart-contract/scripts/deploy.ts
@@ -17,13 +17,20 @@ const verify = async (contractAddress: string, args: any[]) => {
 };
 
 async function main() {
-  const SimpleStorage = await ethers.getContractFactory("SimpleStorage");
-  const simpleStorage = await SimpleStorage.deploy();
+  const Raffle = await ethers.getContractFactory("Raffle");
+  const raffle = await Raffle.deploy(
+    "0x2Ca8E0C643bDe4C2E08ab1fA0da3401AdAD7734D",
+    ethers.utils.parseEther("0.01"),
+    "0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15",
+    "1454",
+    "50000",
+    "60"
+  );
 
-  await simpleStorage.deployed();
-  await verify(simpleStorage.address, []);
+  await raffle.deployed();
+  await verify(raffle.address, []);
   console.log("Verified");
-  console.log(simpleStorage.address);
+  console.log(raffle.address);
 }
 
 main().catch((error) => {
