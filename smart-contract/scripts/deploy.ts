58e04d26cf717e3b2c9b4c1a0263ef71d8513af1
Qreamville <Oyeleyebalikis@yahoo.com>
2022-09-17 06:58:30 -1200

    Update deploy.ts

--- smart-contract/scripts/deploy.ts
+++ smart-contract/scripts/deploy.ts
@@ -18,22 +18,30 @@ const verify = async (contractAddress: string, args: any[]) => {
 
 async function main() {
   const Raffle = await ethers.getContractFactory("Raffle");
-  const raffle = await Raffle.deploy(
+  // const raffle = await Raffle.deploy(
+  //   "0x2Ca8E0C643bDe4C2E08ab1fA0da3401AdAD7734D",
+  //   ethers.utils.parseEther("0.01"),
+  //   "0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15",
+  //   "1454",
+  //   "50000",
+  //   "60"
+  // );
+
+  // await raffle.deployed();
+  await verify("0xe7B770088050Cba5A07bfDbE8e7F1172F0B570A4", [
     "0x2Ca8E0C643bDe4C2E08ab1fA0da3401AdAD7734D",
     ethers.utils.parseEther("0.01"),
     "0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15",
     "1454",
     "50000",
-    "60"
-  );
-
-  await raffle.deployed();
-  await verify(raffle.address, []);
+    "60",
+  ]);
   console.log("Verified");
-  console.log(raffle.address);
+  // console.log(raffle.address);
 }
 
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
 });
+//  0xe7B770088050Cba5A07bfDbE8e7F1172F0B570A4
