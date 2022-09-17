d8fa13f5a75430f831027ac1a23e14063fafc90a
Qreamville <Oyeleyebalikis@yahoo.com>
2022-09-17 07:04:10 -1200

    Create index.tsx

---
+++ client/pages/index.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Raffle Contract </title>
+        <meta name="description" content="Our raffle contract" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </div>
+  );
+};
+
+export default Home;
